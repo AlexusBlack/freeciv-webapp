a0c5f90b8d5a6433b4bd67b2898b8dc896996893
Alex Chernov <alexusblack@gmail.com>
2022-09-15 14:25:06 +1000

    loading css and text strings

--- app.js
+++ app.js
@@ -1,17 +1,30 @@
+const fs = require('fs');
+const ini = require('ini');
 const express = require('express');
 const app = express();
 const port = 3000;
+
+// Predefined text lines
+const msg= ini.parse(fs.readFileSync('./text.properties', 'utf-8'));
 
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
 
 const props = {
+  debugParam: null,
+
   gaTrackingId: null,
-  trackJsTocken: null
+  trackJsTocken: null,
+  googleSigninClientKey: null,
+  captchaKey: null
 };
 
 app.get('/', (req, res) => {
-  res.render('index', {props, title: 'Test'});
+  res.render('index', {title: 'Homepage', props, msg});
+});
+
+app.get('/webclient/', (req, res) => {
+  res.render('webclient/index', {title: 'Client', props, msg});
 });
 
 app.listen(port, () => {
