b331bbc20de4b238ee00351d3d5eeb0924a0c4b5
Alex Chernov <alexusblack@gmail.com>
2022-09-27 17:14:23 +1000

    using git pages format

--- make-static.sh
+++ make-static.sh
@@ -5,6 +5,7 @@ mkdir docs
 cp -r public/* docs/
 
 wget http://localhost:3000/ -O docs/index.html
+cp CNAME.txt docs/CNAME
 mkdir docs/webclient
 wget http://localhost:3000/webclient/ -O docs/webclient/index.html
 
