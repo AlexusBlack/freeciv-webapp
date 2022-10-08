31e77867cc321268e3c08b0329cd0fd8b1ba0652
Alex Chernov <alexusblack@gmail.com>
2022-10-08 15:27:48 +1000

    including d3 and d3.dag into the bundle

--- build.sh
+++ build.sh
@@ -6,6 +6,8 @@ result_file="public/javascript/webclient.min.js"
 npx uglify-js \
   "${js_dir}"/libs/first/EventAggregator.js \
   "${js_dir}"/libs/first/handlebars.runtime-v4.5.3.js \
+  "${js_dir}"/libs/first/d3.v7.min.js \
+  "${js_dir}"/libs/first/d3-dag.iife.min.js \
   "${js_dir}"/derived/*.js \
   "${js_dir}"/*.js \
   "${js_dir}"/libs/first/jquery-ui.min.js \
